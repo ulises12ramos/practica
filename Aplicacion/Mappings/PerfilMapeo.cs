2e57baa0c439b5696027f66694fe8f52e5902db8
Fernandoramos24 <87905334+Fernandoramos24@users.noreply.github.com>
2023-03-12 23:08:31 -0600

    practica

---
+++ Aplicacion/Mappings/PerfilMapeo.cs
@@ -0,0 +1,21 @@
+﻿using Aplicacion.Feautres.Libros.Comandos.CrearLibro;
+using AutoMapper;
+using Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Mappings
+{
+    public class PerfilMapeo : Profile
+    {
+        public PerfilMapeo()
+        {
+            #region Commands
+            CreateMap<CrearLibro,Libro>();
+            #endregion
+        }
+    }
+}
---
+++ RestFullAPIBiblioteca/ApiWeb/Controllers/v1/LibroController.cs
@@ -0,0 +1,35 @@
+﻿using Aplicacion.Feautres.Libros.Comandos.CrearLibro;
+using Aplicacion.Feautres.Libros.Comandos.EliminarLibro;
+using Aplicacion.Feautres.Libros.Comandos.ModificarLibro;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeb.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class LibroController : BaseApiController
+    {
+        //Post 
+        [HttpPost]
+        public async Task<IActionResult> Post(CrearLibro comando)
+        {
+            return Ok(await Mediator.Send(comando));
+        }
+
+        //put
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, ModificarLibro comando)
+        {
+            if (id != comando.Id)
+                return BadRequest();
+            return Ok(await Mediator.Send(comando));
+        }
+
+        //Delete
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new EliminarLibro { Id = id }));
+        }
+    }
+}
